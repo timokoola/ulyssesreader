02e86d6238bb458fda99b695bfdcedc9d326fdbe
Timo Koola <timo@moarub.com>
2013-02-12 16:28:02 +0200

    new way to split lines

--- bookworm.py
+++ bookworm.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
+import string
 import re
 
-regsplit = re.compile("([^.?!]+[.!?]) ")
 
 
 def handle_line(l):
@@ -14,8 +14,12 @@ def handle_line(l):
 class BookWorm:
     def __init__(self,filename):
         f = open(filename)
-        self.book_array = [handle_line(l) for l in f.readlines()]
+        self.book_array = re.split("\s\s+"," ".join([handle_line(l) for l in f.readlines()]))
         f.close()
+        self.reset()
+
+    def reset(self):
+        self.cut = 140
         self.__line = 0
         self.__char = 0
         self.location = 0
@@ -32,20 +36,47 @@ class BookWorm:
         self.__line = self.__line + 1
         self.__char = 0
 
+    def next_part(self,l):
+        if len(l) > 0:
+            return l[-1]
+        else:
+            return ""
+
+    def splitters(self):
+        s = "\t" + self.to_eol() + "\t"
+        lth = len(self.__curr_tweet)
+        cut = self.cut
+        assert(lth <= cut), "len(%s) = %d > %d" % (self.__curr_tweet, lth, cut)
+        result = [l[1]+self.__char for l in zip(s, xrange(len(s))) if l[0] in ",?.!\t" and (lth + l[1]) < (cut-20)]
+        # no punctuation during next 120 chars
+        if len(result) < 2:
+            result = [l[1]+self.__char for l in zip(s, xrange(len(s))) if l[0] in string.whitespace and (lth + l[1]) < cut]
+        #special case just one long word
+        if len(result) < 2:
+            result = [l[1]+self.__char for l in zip(s, xrange(len(s))) if (lth + l[1]) < cut]
+
+        return result
+
+    def to_eol(self):
+        return self.book_array[self.__line][self.__char:]
+
     def split_line(self):
-        parts = regsplit.findall(self.book_array[self.__line])
-        cut = 140
-        if len(parts) == 0:
-            parts = self.book_array[self.__line].split()
-            cut = 140
-        parts.reverse()
-        while len(self.__curr_tweet) != 0 and len(parts) > 0:
-            added = parts.pop()
-            if len(added) + len(self.__curr_tweet) +1 >= cut:
-                self.store_current()
-            else:
-                self.__char = self.__char + len(added) + 1
-                self.__curr_tweet = self.__curr_tweet+ " " + added
+        split = max(self.splitters())
+        self.__curr_tweet =  self.__curr_tweet  + self.add_slice(split)
+        self.__char = split
+        self.store_current()
+        if len(re.sub("\s+","",self.to_eol())) == 0:
+            self.next_line()
+
+    def add_slice(self,split):
+        if split <= self.__char:
+            return ""
+        else:
+            return  " " + self.book_array[self.__line][self.__char:split]
+    
+
+    def curr_location(self):
+        return (self.__line, self.__char)
 
     def traverse_book(self):
         # if we are at the end
@@ -56,21 +87,21 @@ class BookWorm:
         if len(self.book_array[self.__line]) == 0:
             self.store_current()
             self.next_line()
-        elif len(self.book_array[self.__line][self.__char:]) >= 140:
+        elif len(self.to_eol()) >= 120:
             self.store_current()
             self.split_line()
-        elif len(self.__curr_tweet) + len(self.book_array[self.__line]) >= 140:
-            self.split_line()
         else:
-            self.__curr_tweet = self.__curr_tweet + " " + self.book_array[self.__line][self.__char:]
+            #self.split_line()
+            self.__curr_tweet = (" ".join([self.__curr_tweet,self.to_eol()])).strip()
+            self.store_current()
             self.next_line()
 
     def self_test(self):
         if not self.completed:
-            while not bw.completed:
-                bw.traverse_book()
-        mmx = max(map(len,bw.tweets)) 
-        assert(mmx < 140)
+            while not self.completed:
+                self.traverse_book()
+        mmx = max(map(len,self.tweets)) 
+        assert(mmx <= self.cut)
 
 if __name__ == "__main__":
     bw = BookWorm("pg4300.txt")
--- test/ulyssestests.py
+++ test/ulyssestests.py
@@ -1,16 +1,63 @@
-from bookworm import BookWorm
+from bookworm import BookWorm, handle_line
 import unittest
+import re, string
+#class BasicTests(unittest.TestCase):
+#    def setUp(self):
+#        self.book = BookWorm("pg4300.txt")
+#        while not self.book.completed:
+#            self.book.traverse_book()
+#    def test_first_line(self):
+#        self.assertEqual(self.book.tweets[0][3:],"The Project Gutenberg EBook of Ulysses, by James Joyce")
 
-class BasicTests(unittest.TestCase):
+#    def test_100th_line(self):  
+#        self.assertEqual(self.book.tweets[101],"The Project Gutenberg EBook of Ulysses, by James Joyce")
+
+
+
+#
+# Stephen suffered him to pull out and hold up on show by its corner a
+#dirty crumpled handkerchief. Buck Mulligan wiped the razorblade neatly.
+#Then, gazing over the handkerchief, he said:
+
+import re
+regpunct = re.compile("["+string.punctuation+"]+")
+
+
+class FullBookTests(unittest.TestCase):
     def setUp(self):
+        self.maxDiff = None
         self.book = BookWorm("pg4300.txt")
+
+    def test_all_words(self):
         while not self.book.completed:
             self.book.traverse_book()
-    def test_first_line(self):
-        self.assertEqual(self.book.tweets[0][3:],"The Project Gutenberg EBook of Ulysses, by James Joyce")
+        f = open("pg4300.txt")
+        words = f.readlines()
+        f.close()
+        #words = words.lower()
+        self.words = (" ".join([handle_line(l) for l in words])).lower() 
+        self.words2 = (" ".join(self.book.tweets)).lower()
+        self.words = regpunct.sub(" ",self.words)
+        self.words2 = regpunct.sub(" ",self.words2)
+        self.assertItemsEqual(self.words.split(),self.words2.split())
 
-    def test_100th_line(self):  
-        self.assertEqual(self.book.tweets[101],"The Project Gutenberg EBook of Ulysses, by James Joyce")
+    def test_self(self):
+        self.book.self_test()
+
+    def test_advances(self):
+        loc = self.book.curr_location()
+        while not self.book.completed:
+            self.book.traverse_book()
+            if not self.book.completed:
+                self.assertTrue(loc[0] < self.book.curr_location()[0] or loc[1] < self.book.curr_location()[1], "(%d,%d)" % (loc[0],loc[1]))
+            loc = self.book.curr_location()
+
+    def test_pyjaum_count(self):
+        while not self.book.completed:
+            self.book.traverse_book()
+        l = [b for b in self.book.tweets if b.find("Pyjaum") != -1]
+        self.assertTrue(len(l) == 1)
+
 
 class SplitTests(unittest.TestCase):
     def setUp(self):
@@ -18,12 +65,22 @@ class SplitTests(unittest.TestCase):
         while not self.book.completed:
             self.book.traverse_book()
 
-    def test_first_line(self):
-        self.assertEqual(self.book.tweets[0], """He came over to the gunrest and, thrusting a hand into Stephen's upper pocket, said:""")
-
     def test_length(self):
-        print self.book.tweets
-        self.assertEqual(3,len(self.book.tweets))
+        self.assertEqual(2,len(self.book.tweets))
+
+    def test_buck_mulligan(self):
+        self.assertTrue(self.book.tweets[1].startswith("Buck"))
+
+    def test_all_the_words(self):
+        regspaces = re.compile("\s+")
+        f = open("test/split.txt")
+        words = " ".join(f.readlines())
+        f.close()
+        words = words.lower().strip()
+        words = regspaces.sub(" ",words)
+        words2 = (" ".join(self.book.tweets)).lower().strip()
+        words2 = regspaces.sub(" ",words2)
+        self.assertEqual(words,words2)
 
 if __name__ == "__main__":
     unittest.main()
